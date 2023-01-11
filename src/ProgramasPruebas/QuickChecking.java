5e86c8e27fbef5a25abbc44e289352b9a75b562c
GyllenhaalSP <86900724+GyllenhaalSP@users.noreply.github.com>
2023-01-11 12:48:43 +0100

    Updating fixes in java 17.

--- src/ProgramasPruebas/QuickChecking.java
+++ src/ProgramasPruebas/QuickChecking.java
@@ -1,26 +1,7 @@
 package ProgramasPruebas;
 
-import java.util.Arrays;
-
-import static ProgramasPruebas.metodos.cargarArray;
-import static ProgramasPruebas.metodos.mostrarArray;
-
 public class QuickChecking {
     public static void main(String[] args) {
-        Object[] array = new Object[10];
-        Object[][] multiArray = new Object[10][10];
-        Object[] arrayChars = new Object[10];
 
-        cargarArray(array, 10, "int");
-        cargarArray(multiArray, 10, "int");
-        cargarArray(arrayChars, 10, "double");
-
-
-        mostrarArray("Array: ", array);
-        mostrarArray("MultiArray: ", multiArray);
-        mostrarArray("ArrayChars: ", arrayChars);
-
-        System.out.println(Arrays.toString(array));
-        System.out.println(Arrays.deepToString(multiArray));
     }
 }
